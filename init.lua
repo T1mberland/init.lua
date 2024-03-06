e5a055f25e7367c8e452a75c3fb9b5d7a72f3132
T1mberland <104497753+T1mberland@users.noreply.github.com>
2024-03-06 16:00:02 +0900

    init

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- Set up encoding
+vim.opt.encoding = "utf-8"
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamed"
+
+-- Search options
+vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.smartcase = true -- Override 'ignorecase' when search contains uppercase characters
+vim.opt.incsearch = true -- Shows the match while typing
+
+-- UI options
+vim.opt.laststatus = 2 -- Always display the status line
+vim.opt.ruler = true -- Show the cursor position all the time
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line number
+
+-- Editing options
+vim.opt.autoindent = true -- Copy indent from current line when starting a new line
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.softtabstop = 4 -- Number of spaces in tab when editing
+
+-- Set leader key
+vim.g.mapleader = " "
+
+-- Additional settings
+vim.opt.scrolloff = 8 -- Minimum number of screen lines to keep above and below the cursor
+vim.opt.formatoptions:append("mM") -- Don't insert line breaks in the middle of multibyte characters
+vim.opt.ambiwidth = "double" -- Make ambiguous-width characters twice as wide (useful for some Unicode characters)
+vim.opt.display:append("lastline") -- Try to show as much as possible of the last line
+
+-- Enable filetype plugins and indenting
+vim.cmd [[filetype plugin indent on]]
+
+-- Enable syntax highlighting
+vim.cmd [[syntax on]]
+
+-- Set leader key
+vim.g.mapleader = " "
+
+require('keymaps')
+require('plugins')
+require('commands')
+
+-- 
+
---
+++ lua/keymaps.lua
@@ -0,0 +1,43 @@
+-- =============== KEY MAPS ===============
+
+local opts = { noremap = true, silent = true }
+
+-- Moving by display lines
+vim.api.nvim_set_keymap('n', 'j', 'gj', opts)
+vim.api.nvim_set_keymap('n', 'k', 'gk', opts)
+vim.api.nvim_set_keymap('n', '<Down>', 'gj', opts)
+vim.api.nvim_set_keymap('n', '<Up>', 'gk', opts)
+
+-- Reverse to move by actual lines
+vim.api.nvim_set_keymap('n', 'gj', 'j', opts)
+vim.api.nvim_set_keymap('n', 'gk', 'k', opts)
+
+-- Various other normal mode mappings
+vim.api.nvim_set_keymap('n', 'J', 'mzJ`z', opts)
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', opts)
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', opts)
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', opts)
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', opts)
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-d>', opts)
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-u>', opts)
+vim.api.nvim_set_keymap('n', '<Leader>s', ':%s/\\<<C-r><C-w>\\>/\\<<C-r><C-w>\\>/gI<Left><Left><Left>', opts)
+vim.api.nvim_set_keymap('n', '<C-d>', '"_dd', opts)
+vim.api.nvim_set_keymap('n', '<C-x>', '"_x', opts)
+vim.api.nvim_set_keymap('n', 'K', '<nop>', opts)
+
+-- Disable <C-o> in normal mode
+vim.api.nvim_set_keymap('n', '<C-o>', '<nop>', opts)
+
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', opts)
+
+
+-- Moving in visual mode
+vim.api.nvim_set_keymap('v', '<C-j>', '<C-d>', opts)
+vim.api.nvim_set_keymap('v', '<C-k>', '<C-u>', opts)
+vim.api.nvim_set_keymap('v', '<C-d>', '"_d', opts)
+vim.api.nvim_set_keymap('v', '<C-x>', '"_x', opts)
+vim.api.nvim_set_keymap('v', '<C-o>', ':m \'<-2<CR>gv=gv', opts)
+vim.api.nvim_set_keymap('v', '<C-i>', ':m \'>+1<CR>gv=gv', opts)
+vim.api.nvim_set_keymap('v', 'J', '<nop>', opts)
+vim.api.nvim_set_keymap('v', 'K', '<nop>', opts)
+
