65abea14a92c4d6e69f8a8526bfb8aeddf93e77d
T1mberland <ymokc1120@gmail.com>
2024-09-24 09:10:00 +0900

    split plug config

--- lua/plugins/airline.lua
+++ lua/plugins/airline.lua
@@ -1,15 +1,4 @@
 return {
-    {"vim-airline/vim-airline-themes", },
-    {
-      "vim-airline/vim-airline",
-      config = function()
-        vim.g.Powerline_symbols = 'fancy'
-        vim.g.airline_theme = 'base16'
-        vim.g.airline_powerline_fonts = 1
-        vim.g.webdevicons_enable_airline_tabline = 0
-        vim.g['airline#extensions#tabline#enabled'] = 1
-        vim.api.nvim_set_keymap('n', '<C-l>', ':bnext', {silent = true})
-        vim.api.nvim_set_keymap('n', '<C-h>', ':bprev', {silent = true})
-      end,
-    },
+    {"vim-airline/vim-airline-themes" },
+    { "vim-airline/vim-airline" }
 }
