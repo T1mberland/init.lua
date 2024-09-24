fe5c7acb670d5529a1088603dba5d5277c12c785
T1mberland <104497753+T1mberland@users.noreply.github.com>
2024-09-24 09:10:00 +0900

    split plug config

---
+++ lua/plugsetup/init.lua
@@ -0,0 +1,8 @@
+require('plugsetup.clever_f_setup')
+require('plugsetup.airline_setup')
+require('plugsetup.harpoon_setup')
+require('plugsetup.quickscope_setup')
+require('plugsetup.telescope_setup')
+require('plugsetup.linter_setup')
+require('plugsetup.cmp_setup')
+
