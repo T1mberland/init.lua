92cc49ac103a7c4b9edb2c14476cde48b2b8cd5c
T1mberland <ymokc1120@gmail.com>
2024-10-18 20:31:13 +0900

    haskell-tools.nvim

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,7 +2,7 @@ return {
     { "williamboman/mason.nvim", opts = {} },
     { "williamboman/mason-lspconfig.nvim",
       opts = {
-        ensure_installed={ "lua_ls", "rust_analyzer", "tinymist", "hls" }
+        ensure_installed={ "lua_ls", "rust_analyzer", "tinymist" }
       },
       dependencies = { "williamboman/mason.nvim" },
     },
