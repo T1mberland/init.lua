44832d6c3c51075dd5d5bc5fb3db4041d4d26591
T1mberland <ymokc1120@gmail.com>
2024-11-08 19:46:33 +0900

    aaa

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -29,6 +29,8 @@ return {
          'hrsh7th/cmp-buffer',
          'hrsh7th/cmp-path',
          'hrsh7th/cmp-cmdline',
+         'hrsh7th/cmp-nvim-lsp-document-symbol',
+         'hrsh7th/cmp-nvim-lsp-signature-help',
         },
     }
 }
--- lua/plugsetup/airline_setup.lua
+++ lua/plugsetup/airline_setup.lua
@@ -1,7 +1,9 @@
-vim.g.Powerline_symbols = 'fancy'
+--vim.g.airline_symbols = 'fancy'
+vim.g.airline_symbols_ascii = 0
 vim.g.airline_theme = 'base16'
 vim.g.airline_powerline_fonts = 1
-vim.g.webdevicons_enable_airline_tabline = 0
+vim.g.webdevicons_enable_airline_tabline = 1
 vim.g['airline#extensions#tabline#enabled'] = 1
-vim.api.nvim_set_keymap('n', '<C-l>', ':bnext', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-h>', ':bprev', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', ':bnext<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-h>', ':bprev<CR>', {silent = true})
+
