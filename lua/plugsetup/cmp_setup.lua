1cfa9fdce3a4c63e610c358b4e5883c036f2282e
T1mberland <ymokc1120@gmail.com>
2024-11-09 22:01:06 +0900

    f

--- lua/plugsetup/cmp_setup.lua
+++ lua/plugsetup/cmp_setup.lua
@@ -20,7 +20,7 @@ cmp.setup({
     --['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     --['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -116,6 +116,10 @@ require('lspconfig')['tinymist'].setup {
   on_attach = on_attach
 }
 require('lspconfig')['hls'].setup {
+  capabilities = capabilities,
+  on_attach = on_attach
+}
+require('lspconfig')['pylsp'].setup {
   capabilities = capabilities,
   on_attach = on_attach
 }
