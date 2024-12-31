9ae37931711e8d1535aede1d005a6d78e287a898
T1mberland <ymokc1120@gmail.com>
2024-12-31 23:12:12 +0900

    added conform

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,7 +20,17 @@ return {
     },
     {
         'stevearc/conform.nvim',
-        opts = {},
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                -- Conform will run multiple formatters sequentially
+                python = { "ruff" },
+                -- You can customize some of the format options for the filetype (:help conform.format)
+                rust = { "rustfmt", lsp_format = "fallback" },
+                -- Conform will run the first available formatter
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+            },
+        },
     },
     {
         'hrsh7th/nvim-cmp',
