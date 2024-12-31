49d64d1df2fa33a335de7c4beeb3e442aaa1f177
T1mberland <ymokc1120@gmail.com>
2024-12-31 22:55:31 +0900

    formatter (failed)

---
+++ lua/plugsetup/formatter_setup.lua
@@ -0,0 +1,38 @@
+-- Utilities for creating configurations
+local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  -- Set the log level
+  log_level = vim.log.levels.WARN,
+  -- All formatter configurations are opt-in
+  filetype = {
+    rust = {
+      function()
+        return {
+          exe = "stylua",
+          args = {
+            "--search-parent-directories",
+            "--stdin-filepath",
+            util.escape_path(util.get_current_buffer_file_path()),
+            "--",
+            "-",
+          },
+          stdin = true,
+        }
+      end
+    },
+
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+      -- "formatter.filetypes.any" defines default configurations for any
+      -- filetype
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+      -- Remove trailing whitespace without 'sed'
+      -- require("formatter.filetypes.any").substitute_trailing_whitespace,
+    }
+  }
+}
