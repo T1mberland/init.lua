ca6cc098cc2dd1243a29568f175508255a41fff4
T1mberland <104497753+T1mberland@users.noreply.github.com>
2024-12-31 23:14:40 +0900

    added format on save

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,47 +1,53 @@
 return {
-    { "williamboman/mason.nvim", opts = {} },
-    { "williamboman/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed={ "lua_ls", "rust_analyzer", "tinymist" }
-      },
-      dependencies = { "williamboman/mason.nvim" },
-    },
-    {
-        "neovim/nvim-lspconfig",
-      dependencies = {
-          "williamboman/mason.nvim",
-          "williamboman/mason-lspconfig.nvim",
-          "nvim-telescope/telescope.nvim",
-      },
-    },
-    {
-        "mfussenegger/nvim-lint",
-        dependencies = { "williamboman/mason.nvim" },
-    },
-    {
-        'stevearc/conform.nvim',
-        opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python = { "ruff" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                rust = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                javascript = { "prettierd", "prettier", stop_after_first = true },
-            },
-        },
-    },
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-         'neovim/nvim-lspconfig',
-         'hrsh7th/cmp-nvim-lsp',
-         'hrsh7th/cmp-buffer',
-         'hrsh7th/cmp-path',
-         'hrsh7th/cmp-cmdline',
-         'hrsh7th/cmp-nvim-lsp-document-symbol',
-         'hrsh7th/cmp-nvim-lsp-signature-help',
-        },
-    }
+	{ "williamboman/mason.nvim", opts = {} },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "rust_analyzer", "tinymist" },
+		},
+		dependencies = { "williamboman/mason.nvim" },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{
+		"mfussenegger/nvim-lint",
+		dependencies = { "williamboman/mason.nvim" },
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "ruff" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lsp-document-symbol",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+		},
+	},
 }
