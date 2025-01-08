e050529708b9c630a4ab3df1a08fccb7906f76e3
aa@aa.com <aa@aa.com>
2025-01-08 12:36:59 +0900

    few tweaks

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,7 +1,10 @@
 return {
 	"ggandor/leap.nvim",
 	config = function()
-		require("leap").create_default_mappings()
+		--require("leap").create_default_mappings()
+		vim.keymap.set({ "n", "x", "o" }, "e", "<Plug>(leap-forward)")
+		vim.keymap.set({ "n", "x", "o" }, "E", "<Plug>(leap-backward)")
+		vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
 	end,
 	enabled = false,
 }
