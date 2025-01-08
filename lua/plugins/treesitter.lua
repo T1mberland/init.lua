cc516a6564bb39c660eaef36c320789b0eb0a97c
aa@aa.com <aa@aa.com>
2025-01-08 13:53:26 +0900

    disabled severall plugins + set up lazy load

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,21 @@
 return {
-  -- Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    main = 'nvim-treesitter.configs',
-    opts = {
-      highlight = { enable = true },
-      ensure_installed = { "markdown", "lua", "haskell", "python" },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    event = "CursorMoved",
-  },
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		main = "nvim-treesitter.configs",
+		opts = {
+			highlight = { enable = true },
+			ensure_installed = { "markdown", "lua", "haskell", "python" },
+		},
+		event = "BufRead",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		--event = "CursorMoved",
+		event = "BufRead",
+	},
 }
