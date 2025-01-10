1b4e05174542508537ba9cfef86be9b06feb8caa
aa@aa.com <aa@aa.com>
2025-01-10 14:13:56 +0900

    cleaned up

--- lua/plugsetup/airline_setup.lua
+++
@@ -1,9 +0,0 @@
---vim.g.airline_symbols = 'fancy'
-vim.g.airline_symbols_ascii = 0
-vim.g.airline_theme = 'base16'
-vim.g.airline_powerline_fonts = 1
-vim.g.webdevicons_enable_airline_tabline = 1
-vim.g['airline#extensions#tabline#enabled'] = 1
-vim.api.nvim_set_keymap('n', '<C-l>', ':bnext<CR>', {silent = true})
-vim.api.nvim_set_keymap('n', '<C-h>', ':bprev<CR>', {silent = true})
-
