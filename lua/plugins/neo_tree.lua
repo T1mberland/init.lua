54143918b0f6a6e49dbbf5979e2765bf0a1ef499
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-01-10 14:56:34 +0900

    neotree! key bind

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -2,6 +2,14 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	cmd = "Neotree",
+	keys = {
+		{ "<leader>uu", "<cmd>Neotree float reveal toggle<cr>", desc = "NeoTree" },
+		{ "<leader>ui", "<cmd>Neotree left reveal toggle<cr>", desc = "NeoTree" },
+		{ "<leader>ul", "<cmd>Neotree left reveal toggle<cr>", desc = "NeoTree" },
+		{ "<leader>ur", "<cmd>Neotree right reveal toggle<cr>", desc = "NeoTree" },
+
+		{ "<leader>ug", "<cmd>Neotree buffers<cr>", desc = "NeoTree" },
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
