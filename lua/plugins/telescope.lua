138771b892fdc5dae8fdc4de3cf71acfe0f9fe29
aa@aa.com <aa@aa.com>
2025-01-10 14:36:31 +0900

    neotree!

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,13 +9,15 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			--vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
+			--vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		end,
 	},
+	--[[
 	{
 		"nvim-telescope/telescope-file-browser.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 		cmd = "Telescope",
 	},
+  ]]
 }
