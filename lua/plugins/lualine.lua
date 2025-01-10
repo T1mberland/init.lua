dfc0ad601bceee144e698d16b5609d3acd7f8815
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-01-10 16:25:48 +0900

    buffer movements. fixed cmp-setting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,8 +3,16 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VimEnter", -- Load on VimEnter event
 	config = function()
+		vim.api.nvim_set_keymap("n", "<C-l>", ":bnext<CR>", { silent = true })
+		vim.api.nvim_set_keymap("n", "<C-h>", ":bprev<CR>", { silent = true })
+
 		require("lualine").setup({
-			options = { theme = "gruvbox_dark" },
+			options = {
+				--theme = "auto",
+				theme = "gruvbox",
+				globalstatus = true,
+				icons_enabled = true,
+			},
 			-- sections と同列に追記する
 			tabline = {
 				lualine_a = {
