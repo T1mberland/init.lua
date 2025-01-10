1e85da3450c75abae70ace6b8214865f77e4be04
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-01-10 10:54:49 +0900

    lualine

--- lua/plugsetup/init.lua
+++ lua/plugsetup/init.lua
@@ -1,5 +1,5 @@
 require("plugsetup.clever_f_setup")
-require("plugsetup.airline_setup")
+--require("plugsetup.airline_setup")
 --require("plugsetup.harpoon_setup")
 require("plugsetup.quickscope_setup")
 --require("plugsetup.telescope_setup")
