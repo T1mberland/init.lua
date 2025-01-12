e94e8612a402b53bfe4349142188554d6626fca0
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-01-12 12:31:52 +0900

    better keymaps

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,38 +2,33 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VimEnter", -- Load on VimEnter event
-	config = function()
-		vim.api.nvim_set_keymap("n", "<C-l>", ":bnext<CR>", { silent = true })
-		vim.api.nvim_set_keymap("n", "<C-h>", ":bprev<CR>", { silent = true })
-
-		require("lualine").setup({
-			options = {
-				--theme = "auto",
-				theme = "gruvbox",
-				globalstatus = true,
-				icons_enabled = true,
-			},
-			-- sections と同列に追記する
-			tabline = {
-				lualine_a = {
-					{
-						"buffers",
-						buffers_color = switch_color,
-						symbols = { modified = "_󰷥", alternate_file = " ", directory = " " },
-					},
-				},
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {
-					"diff",
-				},
-				lualine_y = {
-					"branch",
-				},
-				lualine_z = {
-					{ "tabs", tabs_color = switch_color },
+	opts = {
+		options = {
+			--theme = "auto",
+			theme = "gruvbox",
+			globalstatus = true,
+			icons_enabled = true,
+		},
+		-- sections と同列に追記する
+		tabline = {
+			lualine_a = {
+				{
+					"buffers",
+					buffers_color = switch_color,
+					symbols = { modified = "_󰷥", alternate_file = " ", directory = " " },
 				},
 			},
-		})
-	end,
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {
+				"diff",
+			},
+			lualine_y = {
+				"branch",
+			},
+			lualine_z = {
+				{ "tabs", tabs_color = switch_color },
+			},
+		},
+	},
 }
