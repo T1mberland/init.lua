9846257dbc8fcc0b301c0d2b733297a0beff73e8
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-08-31 10:22:20 +0900

    fixed hover

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,7 @@
 return {
 	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			--"nvim-telescope/telescope.nvim",
-		},
-	},
-	{
 		"williamboman/mason-lspconfig.nvim",
-		opts = { },
-		dependencies = { {"williamboman/mason.nvim", opts = {} }, "neovim/nvim-lspconfig", },
+		opts = {},
+		dependencies = { { "williamboman/mason.nvim", opts = {} }, "neovim/nvim-lspconfig" },
 	},
 }
