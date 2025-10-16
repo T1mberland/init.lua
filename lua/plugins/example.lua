dd31cd76f70e8f37474287dbbf2f3fe7a940c651
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-10-16 14:24:59 +0900

    feat: add flash.nvim plugin
    Adds the folke/flash.nvim plugin for enhanced navigation, along with custom keybindings.

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -9,6 +9,58 @@ return {
       colorscheme = "gruvbox",
     },
   },
+
+  {
+    "folke/flash.nvim",
+    keys = {
+      -- disable the default flash keymaps
+      { "r", mode = { "n", "x", "o" }, false },
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+      { "<C-s>", mode = { "n", "x", "o" }, false },
+
+      {
+        "e",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "E",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<C-e>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
 }
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
