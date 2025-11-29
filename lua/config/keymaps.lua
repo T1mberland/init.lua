986025c39f26ea6f20270fda3161fe813b704dbe
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-11-29 12:29:40 +0900

    fixed ime issue

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,10 @@ vim.api.nvim_set_keymap("n", "<C-k>", "<C-u>", opts)
 vim.api.nvim_set_keymap("v", "<C-j>", "<C-d>", opts)
 vim.api.nvim_set_keymap("v", "<C-k>", "<C-u>", opts)
 vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
+vim.api.nvim_set_keymap("i", "s", "xi", opts)
+
+vim.cmd([[set iminsert=0]])
+vim.cmd([[set imsearch=0]])
 
 vim.keymap.set("n", "<leader>k", function()
   vim.diagnostic.open_float(nil, { scope = "line", border = "rounded", source = true })
