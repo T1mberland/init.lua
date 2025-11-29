9c9c2bd7a66cb0dc38d215222797314c108e36db
T1mberland <104497753+T1mberland@users.noreply.github.com>
2025-11-29 13:18:30 +0900

    vscode

---
+++ lua/plugins_vscode/quickscope.lua
@@ -0,0 +1,17 @@
+return {
+  "unblevable/quick-scope",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    vim.api.nvim_create_augroup("qs_colors", { clear = true })
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = "qs_colors",
+      pattern = "*",
+      command = "highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline",
+    })
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = "qs_colors",
+      pattern = "*",
+      command = "highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline",
+    })
+  end,
+}
